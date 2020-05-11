ee44392603312140e6f45f8c988a04c86c33afe1
chengang <837718548@qq.com>
2020-05-11 14:09:48 +0800

    添加SBA Client配置

--- src/main/java/com/example/sbaclientdemo/SbaClientDemoApplication.java
+++ src/main/java/com/example/sbaclientdemo/SbaClientDemoApplication.java
@@ -2,6 +2,9 @@ package com.example.sbaclientdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class SbaClientDemoApplication {
@@ -10,4 +13,12 @@ public class SbaClientDemoApplication {
         SpringApplication.run(SbaClientDemoApplication.class, args);
     }
 
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                .and().csrf().disable();
+        }
+    }
 }
